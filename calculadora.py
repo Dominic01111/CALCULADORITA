f05ef3c1badffe07826d0e8cab380318ef093743
Dominic01111 <dominicrack8@gmail.com>
2026-07-22 15:35:47 -0500

    Update calculadora.py
    AGREGAR LA MULTIPLICACIÓN
    DOMINIC NOVELLA

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,4 @@
-## CALCULADORA - VERSIÓN 2
+## CALCULADORA - VERSIÓN 3
 # Autor(a): DOMINIC NOVELLA
 # ===========================================
 print("CALCULADORA")
@@ -6,8 +6,9 @@ print("CALCULADORA")
 num1 = float(input("Ingrese el primer número: "))
 num2 = float(input("Ingrese el segundo número: "))
 
-print("\n 1. Sumar")
+print("\ n1. Sumar")
 print(" 2. Restar")
+print(" 3. Multiplicar")
 
 opcion = input("\n Seleccione una opción: ")
 
@@ -16,6 +17,9 @@ print("\n La suma es:", num1 + num2)
 
 elif opcion == "2":
 print("\n La resta es:", num1 - num2)
+
+elif opcion == "3":
+print("\n La multiplicación es:", num1 * num2)
 
 else:
 print("\n Opción no válida.")
